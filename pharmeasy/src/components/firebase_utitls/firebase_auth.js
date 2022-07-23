1e2259227cc2f7b737de76ecf00487d8d034adf7
md-sahil-a <mdsahilamaan2017@gmail.com>
2022-07-23 10:16:27 +0530

    authentication

---
+++ pharmeasy/src/components/firebase_utitls/firebase_auth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOMC9MveSkwcIbtzvvMCYLaO6MCLFSJG8",
+  authDomain: "pharmeasy-42f81.firebaseapp.com",
+  projectId: "pharmeasy-42f81",
+  storageBucket: "pharmeasy-42f81.appspot.com",
+  messagingSenderId: "209270924071",
+  appId: "1:209270924071:web:9b8461f7c3808df959bf02"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
