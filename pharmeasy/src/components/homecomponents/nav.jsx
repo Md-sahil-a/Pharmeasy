f5b52044dc96c823eb61b0a8aff4773364d0f58d
md-sahil-a <mdsahilamaan2017@gmail.com>
2022-07-23 16:34:00 +0530

    update

---
+++ pharmeasy/src/components/homecomponents/nav.jsx
@@ -0,0 +1,30 @@
+import "../homecomponents/nav.css";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div className="Navbar">
+      <div className="logocontainer">
+        <img
+          alt="logo"
+          className="logo"
+          src="https://assets.pharmeasy.in/web-assets/dist/fca22bc9.png"
+        />
+        <div id="search">
+          <input
+            type="text"
+            id="search_bar"
+            placeholder="search medicines/Healthcare products"
+          />
+          <i class="fa-solid fa-magnifying-glass" id="search_icon"></i>
+        </div>
+      </div>
+      <div className="subNav">
+       
+
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
