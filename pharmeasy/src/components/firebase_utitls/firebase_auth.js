eaa937dab287c1b2f75deb66f42935424aca592c
md-sahil-a <mdsahilamaan2017@gmail.com>
2022-07-23 16:41:52 +0530

    firebase-config file added

--- pharmeasy/src/components/firebase_utitls/firebase_auth.js
+++ pharmeasy/src/components/firebase_utitls/firebase_auth.js
@@ -1,9 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBOMC9MveSkwcIbtzvvMCYLaO6MCLFSJG8",
   authDomain: "pharmeasy-42f81.firebaseapp.com",
@@ -15,3 +21,51 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createrUserFromAuth = async (userAuth, additionalData = {}) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const snapShot = await getDoc(userDocRef);
+  console.log(snapShot.exists());
+  if (!snapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("Error creating the user", error.message);
+    }
+  }
+  return userDocRef;
+};
+
+export const CreateAuthWithEmail = async (email, password) => {
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const Signinwithmail = async (email, password) => {
+  if (!email || !password) return;
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => {
+  return await signOut(auth);
+};
+
+export const onAuthStateChangedListner = (callback) =>
+  onAuthStateChanged(auth, callback);
--- pharmeasy/src/components/homecomponents/nav.jsx
+++ pharmeasy/src/components/homecomponents/nav.jsx
@@ -1,4 +1,4 @@
-import "../homecomponents/nav.css";
+import "../homecomponents/nav.scss";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
